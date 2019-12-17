8d40fee2c8c6fd3e7b6d863f1720aa56d3165086
Martin <mave1700@student.miun.se>
2019-12-17 13:34:15 +0100

    Fixed bug

--- Entities/Models/UserDto.cs
+++ Entities/Models/UserDto.cs
@@ -10,7 +10,6 @@ namespace Entities.Models
         public string Lastname { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Username { get; set; }
-        public string Password { get; set; }
 
         public IEnumerable<TaskDto> Tasks { get; set; }
     }
--- TodoServer2/MappingProfile.cs
+++ TodoServer2/MappingProfile.cs
@@ -8,6 +8,7 @@ namespace TodoServer2
         public MappingProfile()
         {
             CreateMap<User, UserDto>();
+            CreateMap<Task, TaskDto>();
         }
     }
 }
